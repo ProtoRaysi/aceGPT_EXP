589b6844478ba2dd7c433b084644a5e9773f79b9
ProtoRaysi <113710929+ProtoRaysi@users.noreply.github.com>
2024-02-03 14:51:55 +0300

    Update setup_run.sh: install neovim

--- setup_run.sh
+++ setup_run.sh
@@ -1,4 +1,5 @@
 apt update && apt upgrade -y
+apt install neovim -y
 git clone https://github.com/ProtoRaysi/aceGPT_EXP.git && cd aceGPT_EXP
 python -m venv .env && source .env/bin/activate
 pip install -r requirements.txt
